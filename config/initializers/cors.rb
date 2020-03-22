498def2531f69ea02a88b541448e869b1148e94a
“jamalnuman” <jnuman82@gmail.com>
2020-03-22 17:18:48 -0500

    map with route working

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'ngrok.io'
+    origins '*' 
+    # 'localhost:8080', 'ngrok.io'
 
     resource '*',
       headers: :any,
